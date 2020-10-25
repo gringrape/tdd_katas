9259bbb64d53a87a8bd4a7d4de48c14950dd3e05
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-25 14:22:45 +0900

    Add PrimeFactors TDD
    Please enter the commit message for your changes. Lines starting

--- prime_factors/primeFactors.js
+++ prime_factors/primeFactors.js
@@ -3,6 +3,21 @@ export default class PrimeFactors {
     if (number < 2) {
       return [];
     }
-    return [number];
+
+    const factor = this._findFactor(number);
+
+    return [factor, ...this.generate(number / factor)];
+  }
+
+  _findFactor(number, divisor = 2) {
+    if (number % divisor === 0) {
+      return divisor;
+    }
+
+    return this._findFactor(number, divisor + 1);
   }
 }
+
+// 염두에 둔것
+// 1. 하나의 함수는 하나의 역할만 한다.
+// 2. 추상화 계층은 하나로 유지한다.
--- prime_factors/primeFactors.test.js
+++ prime_factors/primeFactors.test.js
@@ -11,5 +11,25 @@ describe('primeFactors', () => {
     it('should return [2] with 2', () => {
       expect(primeFactors.generate(2)).toEqual([2]);
     });
+
+    it('should return [2, 2] with 4', () => {
+      expect(primeFactors.generate(4)).toEqual([2, 2]);
+    });
+
+    it('should return [2, 3] with 6', () => {
+      expect(primeFactors.generate(6)).toEqual([2, 3]);
+    });
+
+    it('should return [2, 2, 2] with 8', () => {
+      expect(primeFactors.generate(8)).toEqual([2, 2, 2]);
+    });
+
+    it('should return [3, 3] with 9', () => {
+      expect(primeFactors.generate(9)).toEqual([3, 3]);
+    });
+
+    it('should return [2, 2, 3, 5, 7, 11] with 4620', () => {
+      expect(primeFactors.generate(4620)).toEqual([2, 2, 3, 5, 7, 11]);
+    });
   });
 });
