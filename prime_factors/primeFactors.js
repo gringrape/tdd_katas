e0c8b21072cd0422520000694d879d7d60eeaaca
gringrape <53764714+gringrape@users.noreply.github.com>
2020-11-01 15:47:30 +0900

    complete

--- prime_factors/primeFactors.js
+++ prime_factors/primeFactors.js
@@ -4,20 +4,19 @@ export default class PrimeFactors {
       return [];
     }
 
-    const factor = this._findFactor(number);
+    const smallestFactor = this._findSmallestFactor(number);
 
-    return [factor, ...this.generate(number / factor)];
+    return [
+      smallestFactor,
+      ...this.generate(number / smallestFactor)
+    ];
   }
 
-  _findFactor(number, divisor = 2) {
-    if (number % divisor === 0) {
-      return divisor;
+  _findSmallestFactor(number, acc = 2) {
+    if (number % acc === 0) {
+      return acc;
     }
 
-    return this._findFactor(number, divisor + 1);
+    return this._findSmallestFactor(number, acc + 1);
   }
 }
-
-// 염두에 둔것
-// 1. 하나의 함수는 하나의 역할만 한다.
-// 2. 추상화 계층은 하나로 유지한다.
--- prime_factors/primeFactors.test.js
+++ prime_factors/primeFactors.test.js
@@ -1,35 +1,35 @@
 import PrimeFactors from './primeFactors';
 
-describe('primeFactors', () => {
-  const primeFactors = new PrimeFactors();
-  
+describe('PrimeFactors', () => {
   describe('generate', () => {
-    it('should return [] with 1', () => {
+    it('returns [] with 1', () => {
+      const primeFactors = new PrimeFactors();
+    
       expect(primeFactors.generate(1)).toEqual([]);
     });
 
-    it('should return [2] with 2', () => {
+    it('returns [2] with 2', () => {
+      const primeFactors = new PrimeFactors();
+    
       expect(primeFactors.generate(2)).toEqual([2]);
     });
 
-    it('should return [2, 2] with 4', () => {
+    it('returns [2, 2] with 4', () => {
+      const primeFactors = new PrimeFactors();
+    
       expect(primeFactors.generate(4)).toEqual([2, 2]);
     });
 
-    it('should return [2, 3] with 6', () => {
+    it('returns [2, 3] with 6', () => {
+      const primeFactors = new PrimeFactors();
+    
       expect(primeFactors.generate(6)).toEqual([2, 3]);
     });
 
-    it('should return [2, 2, 2] with 8', () => {
+    it('returns [2, 2, 2] with 8', () => {
+      const primeFactors = new PrimeFactors();
+    
       expect(primeFactors.generate(8)).toEqual([2, 2, 2]);
-    });
-
-    it('should return [3, 3] with 9', () => {
-      expect(primeFactors.generate(9)).toEqual([3, 3]);
-    });
-
-    it('should return [2, 2, 3, 5, 7, 11] with 4620', () => {
-      expect(primeFactors.generate(4620)).toEqual([2, 2, 3, 5, 7, 11]);
     });
   });
 });
